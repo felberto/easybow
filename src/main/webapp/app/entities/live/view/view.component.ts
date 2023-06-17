19b3d702a67d52670afcd4057780e67ab633bec5
felberto <tobias.felber97@gmail.com>
2023-06-17 11:22:47 +0200

    fix live view

--- src/main/webapp/app/entities/live/view/view.component.ts
+++ src/main/webapp/app/entities/live/view/view.component.ts
@@ -4,6 +4,7 @@ import { ICompetition } from '../../competition/competition.model';
 import { RankingListService } from '../../competition/service/rankingList.service';
 import { IRankingList } from '../../competition/rankingList.model';
 import { IAthleteResult } from '../../competition/athleteResult.model';
+import { AccountService } from '../../../core/auth/account.service';
 
 @Component({
   selector: 'jhi-view',
@@ -17,14 +18,25 @@ export class ViewComponent implements OnInit {
   rankingListView: IAthleteResult[] = [];
   rank = 0;
 
+  liveUser = false;
+
   variable = 1;
 
-  constructor(protected activatedRoute: ActivatedRoute, private rankingListService: RankingListService) {}
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    private rankingListService: RankingListService,
+    protected accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ competition }) => {
       this.competition = competition;
       if (this.competition != null) {
+        this.accountService.getAuthorites().forEach(role => {
+          if (role === 'ROLE_LIVE') {
+            this.liveUser = true;
+          }
+        });
         this.refreshData();
         setInterval(() => {
           this.refreshData();
@@ -38,15 +50,19 @@ export class ViewComponent implements OnInit {
       this.rankingListService.getRankingList(this.competition, 99).subscribe(res => {
         console.log(res.body);
         this.rankingList = res.body;
-        if (this.rankingList!.athleteResultList!.length > 10) {
-          if (this.variable === 1) {
-            this.rankingListView = this.rankingList!.athleteResultList!.slice(0, 10);
-            this.variable = 2;
-            this.rank = 0;
+        if (this.liveUser) {
+          if (this.rankingList!.athleteResultList!.length > 10) {
+            if (this.variable === 1) {
+              this.rankingListView = this.rankingList!.athleteResultList!.slice(0, 10);
+              this.variable = 2;
+              this.rank = 0;
+            } else {
+              this.rankingListView = this.rankingList!.athleteResultList!.slice(10, 21);
+              this.variable = 1;
+              this.rank = 1;
+            }
           } else {
-            this.rankingListView = this.rankingList!.athleteResultList!.slice(10, 21);
-            this.variable = 1;
-            this.rank = 1;
+            this.rankingListView = this.rankingList!.athleteResultList!;
           }
         } else {
           this.rankingListView = this.rankingList!.athleteResultList!;
